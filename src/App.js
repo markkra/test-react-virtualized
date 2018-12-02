e3efe7fdd434fbd202a678b47968523bd9755c2b
Mark Kramer <markkra@outlook.com>
2018-12-02 15:51:39 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import deepFreeze from 'deep-freeze';
+import './App.css';
+import Lister from './lister';
+
+const FETCH_SIZE = 12;
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      listItems: deepFreeze(this.createMockData(100000)),
+      list: [],
+      hasMore: false,
+      isLoading: false
+    };
+
+    this.createMockData = this.createMockData.bind(this);
+    this.getPage = this.getPage.bind(this);
+    this.loadNextPage = this.loadNextPage.bind(this);
+  }
+
+  render() {
+    return (
+      <Lister
+        list={this.state.list}
+        hasMore={this.state.hasMore}
+        isNextPageLoading={this.state.isLoading}
+        loadNextPage={this.loadNextPage}
+      />
+    );
+  }
+
+  componentDidMount() {
+    this.getPage(0, FETCH_SIZE).then(data =>
+      this.setState({
+        list: data.list,
+        hasMore: data.hasMore,
+        nextIndex: data.nextIndex,
+        isLoading: false
+      })
+    );
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // console.log(prevState);
+    // console.log(this.state);
+  }
+
+  loadNextPage() {
+    if (this.state.hasMore) {
+      this.getPage(this.state.nextIndex, this.state.nextIndex + FETCH_SIZE).then(data =>
+        this.setState({
+          list: [...this.state.list, ...data.list],
+          hasMore: data.hasMore,
+          nextIndex: data.nextIndex,
+          isLoading: false
+        })
+      );
+    }
+  }
+
+  getPage(start, end) {
+    console.log(`Loading rows ${start} to ${end}`);
+    this.setState({ isLoading: true });
+    return new Promise(resolve => {
+      const results = {
+        list: this.state.listItems.slice(start, end),
+        hasMore: end < this.state.listItems.length,
+        nextIndex: end
+      };
+      setTimeout(() => {
+        resolve(results);
+      }, 500);
+    });
+  }
+
+  createMockData(countItems) {
+    const listItems = [];
+    for (let i = 0; i < countItems; i++) {
+      listItems.push(`Row ${i}`);
+    }
+    return listItems;
+  }
+}
+
+export default App;
---
+++ src/lister.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { AutoSizer, InfiniteLoader, List } from 'react-virtualized';
+import InfiniteScroll from 'react-infinite-scroller';
+import 'react-virtualized/styles.css'; // only needs to be imported once
+import './lister.styles.css';
+
+function LoadingPlaceholder({ items }) {
+  return (
+    <>
+      <div className="content-placeholder-text">
+        <span className="content-placeholder-background" />
+      </div>
+      <div className="content-placeholder-text">
+        <span className="content-placeholder-background" />
+      </div>
+    </>
+  );
+}
+
+function Lister({ list, hasMore, isNextPageLoading, loadNextPage }) {
+  const rowCount = (list && list.length) || 0;
+
+  // Only load 1 page of items at a time.
+  // Pass an empty callback to InfiniteLoader in case it asks us to load more than once.
+  const loadMoreRows = hasMore ? (isNextPageLoading ? () => {} : loadNextPage) : () => {};
+
+  // Every row is loaded except for our loading indicator row.
+  const isRowLoaded = ({ index }) => !hasMore || index < list.size;
+
+  function rowRenderer({
+    index, // Index of row
+    isScrolling, // The List is currently being scrolled
+    isVisible, // This row is visible within the List (eg it is not an overscanned row)
+    key, // Unique key within array of rendered rows
+    parent, // Reference to the parent List (instance)
+    style // Style object to be applied to row (to position it);
+    // This must be passed through to the rendered row element.
+  }) {
+    const conversationItem = list[index];
+
+    // If row content is complex, consider rendering a light-weight placeholder while scrolling.
+    const content = isScrolling ? <LoadingPlaceholder /> : conversationItem;
+
+    // Style is required since it specifies how the row is to be sized and positioned.
+    // React Virtualized depends on this sizing/positioning for proper scrolling behavior.
+    // By default, the List component provides following style properties:
+    //    position
+    //    left
+    //    top
+    //    height
+    //    width
+    // You can add additional class names or style properties as you would like.
+    // Key is also required by React to more efficiently manage the array of rows.
+    return (
+      <div key={key} style={style}>
+        <div className="row">{content}</div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="container">
+      <InfiniteLoader isRowLoaded={isRowLoaded} loadMoreRows={loadMoreRows} rowCount={rowCount}>
+        {({ onRowsRendered, registerChild }) => (
+          <div className="List">
+            <AutoSizer>
+              {({ height, width }) => (
+                <List
+                  ref={registerChild}
+                  onRowsRendered={onRowsRendered}
+                  height={height}
+                  rowCount={rowCount}
+                  rowHeight={40}
+                  rowRenderer={rowRenderer}
+                  width={width}
+                />
+              )}
+            </AutoSizer>
+          </div>
+        )}
+      </InfiniteLoader>
+    </div>
+  );
+}
+
+export { Lister };
+export default Lister;
